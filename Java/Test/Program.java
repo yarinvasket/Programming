192bd8f2684016ed52e5ba0fd87014688209bc82
vasker <yarinvasket@gmail.com>
2019-10-31 23:20:50 +0200

    modified:   Java/Test/Program.java

--- Java/Test/Program.java
+++ Java/Test/Program.java
@@ -1,7 +1,18 @@
 package Test;
 
+import java.util.List;
+import java.util.ArrayList;
+
 class Program {
     public static void main(String[] args) {
-        System.out.println("Shut the fuck up");
+        List<Integer> primeNumbers = new ArrayList<Integer>();
+        primeNumbers.add(2);
+
+        inner: for (int i = 3;; i++) {
+            for (int e : primeNumbers)
+                if (i % e == 0)
+                    continue inner;
+            primeNumbers.add(i);
+        }
     }
 }
