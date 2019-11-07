3bb8b99a7887f8f76f62253a3c39ed52833c82d4
vasker <yarinvasket@gmail.com>
2019-11-07 18:01:05 +0200

    new file:   Java/Recursive/Program.java

---
+++ Java/Recursive/Program.java
@@ -0,0 +1,11 @@
+package Recursive;
+
+public class Program {
+    public static float CalcSum(int num) {
+        return CalcSum(num, 0);
+    }
+
+    public static float CalcSum(int num, int i) {
+
+    }
+}
