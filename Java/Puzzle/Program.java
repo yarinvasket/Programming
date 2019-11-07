cc6328b53e7a0b14760bf15625cf2b1fbf5c2508
vasker <yarinvasket@gmail.com>
2019-11-07 18:00:32 +0200

    new file:   Java/Puzzle/Program.java

---
+++ Java/Puzzle/Program.java
@@ -0,0 +1,31 @@
+package Puzzle;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Program {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int[] puzzle(int people, int freq, int surivors) {
+        Map<Integer, Boolean> nums = new HashMap<Integer, Boolean>();
+
+        for (int i = 0; i < people; i++) {
+            nums.put(i, false);
+        }
+
+        int res;
+
+        for (res = 0; !nums.isEmpty(); res += freq) {
+            if (res >= people) {
+                res %= people;
+            }
+            nums.remove(res);
+        }
+        if (res >= people) {
+            res %= people;
+        }
+        System.out.println(res);
+    }
+}
