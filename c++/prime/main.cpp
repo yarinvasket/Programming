23caa238e4416aedde3497d1ab856ffe7ffe275c
vasker <yarinvasket@gmail.com>
2020-02-28 15:40:44 +0200

    final

--- c++/prime/main.cpp
+++ c++/prime/main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include "LinkedList.hpp"
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    cout << "2\n3\n";
+    
+    LinkedList * primeNumbers = new LinkedList(3);
+    for (int i = 5;; i += 2)
+    {
+        Node * curr = primeNumbers->head;
+        while (1)
+        {
+            if (!curr->next)
+            {
+                if (i % curr->data != 0)
+                {
+                    primeNumbers->add(i);
+                    cout << i << "\n";
+                }
+                break;
+            }
+            
+            if (i % curr->data == 0)
+            {
+                break;
+            }
+            curr = curr->next;
+        }
+    }
     return 0;
 }
