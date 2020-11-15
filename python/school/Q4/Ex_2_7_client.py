c9b92d6c6280afd4c9d43b379ba638390dbacd3d
vasker <yarinvasket@gmail.com>
2020-11-15 17:04:25 +0200

    working version but crushing

---
+++ python/school/Q4/Ex_2_7_client.py
@@ -0,0 +1,75 @@
+#   Ex. 2.7 template - client side
+#   Author: Barak Gonen, 2017
+#   Modified for Python 3, 2020
+
+
+import socket
+
+IP = '127.0.0.1'
+PORT = 9840
+
+
+def valid_request(request):
+	"""Check if the request is valid (is included in the available commands)
+
+	Return:
+	True if valid, False if not
+	"""
+	commands = ['TAKE_SCREENSHOT', 'SEND_FILE', 'DIR',
+				'DELETE',  'COPY',  'EXECUTE',  'EXIT']
+	valid = False
+
+	if request in commands:
+		valid = True
+
+	return True
+	
+
+def send_request_to_server(my_socket, request):
+	"""Send the request to the server. First the length of the request (2 digits), then the request itself
+
+	Example: '04EXIT'
+	Example: '12DIR c:\cyber'
+	"""
+	my_socket.send(request.encode())
+
+
+def handle_server_response(my_socket, request):
+	"""Receive the response from the server and handle it, according to the request
+
+	For example, DIR should result in printing the contents to the screen,
+	while SEND_FILE should result in saving the received file and notifying the user
+	"""
+	if request.split()[0] == 'SEND_FILE':
+		f = open(r'D:\screen2.jpg', 'wb')
+		l = my_socket.recv(1024)
+		while l:
+			f.write(l)
+			l = my_socket.recv(1024)
+		f.close()
+	else:
+		print(my_socket.recv(1024).decode())
+
+def main():
+	# open socket with the server
+	my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	my_socket.connect((IP, PORT))
+
+	# print instructions
+	print('Welcome to remote computer application. Available commands are:\n')
+	print('TAKE_SCREENSHOT\nSEND_FILE\nDIR\nDELETE\nCOPY\nEXECUTE\nEXIT')
+
+	# loop until user requested to exit
+	while True:
+		request = input("Please enter command:\n")
+		if valid_request(request):
+			print("Command is valid")
+			pass
+			send_request_to_server(my_socket, request)
+			handle_server_response(my_socket, request)
+			if request == 'EXIT':
+				break
+	my_socket.close()
+
+if __name__ == '__main__':
+	main()
---
+++ python/school/Q4/Ex_2_7_server.py
@@ -0,0 +1,104 @@
+#   Ex. 2.7 template - server side
+#   Author: Barak Gonen, 2017
+#   Modified for Python 3, 2020
+
+import socket
+from PIL import ImageGrab
+import os.path
+
+IP = '0.0.0.0'
+PORT = 9840
+
+
+def receive_client_request(client_socket):
+	"""Receives the full message sent by the client
+
+	Works with the protocol defined in the client's "send_request_to_server" function
+
+	Returns:
+		command: such as DIR, EXIT, SCREENSHOT etc
+		params: the parameters of the command
+
+	Example: 12DIR c:\cyber as input will result in command = 'DIR', params = 'c:\cyber'
+	"""
+
+	data = client_socket.recv(1024).decode().split()
+	command = data[0]
+	if len(data) <= 1:
+		params = None
+	else:
+		params = data[1::]
+
+	return command, params
+
+
+def check_client_request(command, params):
+	"""Check if the params are good.
+
+	For example, the filename to be copied actually exists
+
+	Returns:
+		valid: True/False
+		error_msg: None if all is OK, otherwise some error message
+	"""
+	if command == 'SEND_FILE':
+		return os.path.isfile(params[0]), 'Success'
+	return True, 'Failure'
+
+
+def handle_client_request(command, params):
+	"""Create the response to the client, given the command is legal and params are OK
+
+	For example, return the list of filenames in a directory
+
+	Returns:
+		response: the requested data
+	"""
+	if command == 'TAKE_SCREENSHOT':
+		im = ImageGrab.grab()
+		im.save(r'D:\screen.jpg')
+		return 'screenshot taken'
+	elif command == 'SEND_FILE':
+		return params
+
+def send_response_to_client(command, response, client_socket):
+	"""Create a protocol which sends the response to the client
+
+	The protocol should be able to handle short responses as well as files
+	(for example when needed to send the screenshot to the client)
+	"""
+	if command == 'SEND_FILE':
+		f = open(r'D:\screen.jpg', 'rb')
+		l = f.read(1024)
+		while (l):
+			client_socket.send(l)
+			l = f.read(1024)
+	else:
+		client_socket.send(response.encode())
+
+
+def main():
+	# open socket with client
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_socket.bind((IP, PORT))
+	server_socket.listen()
+	client_socket, address = server_socket.accept()
+
+	# handle requests until user asks to exit
+	while True:
+		command, params = receive_client_request(client_socket)
+		valid, error_msg = check_client_request(command, params)
+		if valid:
+			response = handle_client_request(command, params)
+			send_response_to_client(command, response, client_socket)
+		else:
+			send_response_to_client(error_msg, client_socket)
+
+		if command == 'EXIT':
+			break
+
+	client_socket.close()
+	server_socket.close()
+
+if __name__ == '__main__':
+	main()
