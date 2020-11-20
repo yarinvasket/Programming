d1f4f4bb1a514cd3aa8653a05f59ed89f47653c3
vasker <yarinvasket@gmail.com>
2020-11-21 01:16:44 +0200

    fix remove_adjacent

--- python/school/Q5/list2.py
+++ python/school/Q5/list2.py
@@ -12,11 +12,16 @@
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
 def remove_adjacent(nums):
-	for i in range(1, len(nums)):
+	if (len(nums) == 0):
+		return nums
+
+	i = 1
+	while i < len(nums):
 		if (nums[i - 1] == nums[i]):
-			nums.remove(i)
-			i -= 1
+			nums.remove(nums[i])
 		else:
+			i += 1
+	return nums
 			
 
 
