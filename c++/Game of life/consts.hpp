548d948f8fac80c4a138d8ff35fe3e2ed8948e03
vasker <yarinvasket@gmail.com>
2020-12-04 17:31:22 +0200

    test run time

--- c++/Game of life/consts.hpp
+++ c++/Game of life/consts.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#define HEIGHT 15
-#define WIDTH 15
-#define ARR {{2, 0}, {2, 1}, {2, 2}, {1, 2}, {0, 1}, {9, 9}, {8, 8}, {8, 10}, {7, 9}}
-#define SIZE 9
-#define MOVES 50
+#define HEIGHT 50
+#define WIDTH 50
+#define ARR {{22, 24}, {23, 24}, {23, 26}, {25, 25}, {26, 24}, {27, 24}, {28, 24}}
+#define SIZE 7
+#define MOVES 250
 #define Point std::pair<unsigned int, unsigned int>
--- c++/Game of life/main.cpp
+++ c++/Game of life/main.cpp
@@ -1,16 +1,18 @@
 #include "game.hpp"
 #include "consts.hpp"
 #include <iostream>
+#include <chrono>
 
 int main() {
+	auto startTime = std::chrono::high_resolution_clock::now();
 	const Point arr[] = ARR;
 	Game game(arr, SIZE);
-	std::cout << game.getBoard();
 
 	for (unsigned int i = 0; i < MOVES; i++) {
 		game.doTurn();
-		std::cout << game.getBoard();
 	}
-	std::cin.get();
+	auto endTime = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+	std::cout << "Process done in " << duration << "ms.\n";
 	return 0;
 }
